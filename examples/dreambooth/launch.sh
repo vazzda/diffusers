28751204bc17d947fc40eba0830c14120607437a
Dima Roma <vazzda@gmail.com>
2023-04-18 02:09:11 +0200

    check commit

--- examples/dreambooth/launch.sh
+++ examples/dreambooth/launch.sh
@@ -1,4 +1,5 @@
-export MODEL_NAME="/home/vazzda/sd/_models/runwayml/stable-diffusion-v1-5"
+#export MODEL_NAME="/home/vazzda/sd/_models/runwayml/stable-diffusion-v1-5"
+export MODEL_NAME="/home/vazzda/sd/_models/Deliberate"
 export VAE_NAME="/home/vazzda/sd/_models/stabilityai/sd-vae-ft-mse-original"
 export OUTPUT_DIR="/mnt/c/_wrk/SD/__target/_results"
 
@@ -8,7 +9,7 @@ accelerate launch train_dreambooth.py \
   --output_dir=$OUTPUT_DIR \
   --revision="fp16" \
   --with_prior_preservation --prior_loss_weight=1.0 \
-  --seed=3434550 \
+  --seed=234425310 \
   --resolution=512 \
   --train_batch_size=1 \
   --train_text_encoder \
@@ -19,13 +20,13 @@ accelerate launch train_dreambooth.py \
   --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=300 \
+  --num_class_images=60 \
   --sample_batch_size=1 \
   --n_save_sample=5 \
-  --max_train_steps=3600 \
-  --save_interval=100 \
-  --save_sample_prompt="photo of @zwx woman" \
-  --save_sample_prompt_sanity="photo of @zwx woman by Tomer Hanuka" \
-  --save_sample_prompt_sanity_celeb="photo of Emma Stone" \
-  --save_sample_prompt_sanity_celeb_mod="photo of Emma Stone by Tomer Hanuka" \
-  --concepts_list="concepts_list.json"
+  --max_train_steps=4000 \
+  --save_interval=5 \
+  --concepts_list="concepts_list.json" \
+  --sanity_list="sanity_list.json" \
+  --save_sample_prompt="shoulnt be in use, cause of sanily list" \
+#  --read_prompts_from_txts
+
